b47ae0984534aa736dc34e8b8b600b279fbc61df
+ <+>
2022-07-14 14:51:38 -0400

    Added event listnerer on buttons

--- js/index.js
+++ js/index.js
@@ -1,9 +1,59 @@
 const m = document.querySelectorAll(".select");
 const w = document.querySelector("#watchedlist")
+const c = document.querySelector("#create")
+const f = document.querySelector("#favs")
 console.log(m)
 console.log(w)
 
 document.addEventListener("DOMContentLoaded",handleload)
+
+for (const item of m) {
+
+
+    w.style.display = 'none'
+
+    item.addEventListener('click', e => {
+
+        if (e.target == document.querySelector("#watched.select")) {
+
+            w.style.display = ''
+            c.style.display = 'none'
+            f.style.display = 'none'
+            console.log(item)
+            console.log(e.target)
+
+        } else if (e.target == document.querySelector("#favsList.select")) {
+
+            w.style.display = 'none'
+            c.style.display = 'none'
+            console.log(item)
+            console.log(e.target)
+
+        } else if (e.target == document.querySelector("#input.select")) {
+
+            c.style.display = ''
+            w.style.display = 'none'
+            f.style.display = 'none'
+
+        } else {
+
+            w.style.display = 'none'
+            f.style.display = 'none'
+
+        }
+        
+    }) 
+
+}
+
+
+// const handleSelector = () => {
+
+    
+
+// }
+
+
 
 function handleload() {
 
@@ -15,49 +65,49 @@ function fetchlist() {
     
     fetch("https://api.jikan.moe/v3/search/anime?q=naruto")
     .then(resp => resp.json())
-    .then(list => handelDisplayList(list))
+    .then(list => handleList(list))
     .catch()
 
 }
 
 
-
-function handelDisplayList(obj) {
+function handleList(obj) {
 
     l = obj.results
 
     l.forEach(element => {
         
-        maketile(element)
+        makeTile(element)
         
     });
 
 }
 
-function maketile(element) {
+function makeTile(element) {
 
     const div = document.createElement("div")
     div.id = `Anime # ${element.id}`
     div.className = 'anime-list'
 
-    const bg = document.createElement('img')
-    bg.innerContent = element.image_url
+    const img = document.createElement('img')
+    img.src = element.image_url
 
-    const  title = document.createElement("h2")
+    const  title = document.createElement("h1")
     title.textContent = element.title
 
-    const span = document.createElement("span")
-    span.className = "details"
-    span.textContent = `
+    const ul = document.createElement("ul")
+    ul.className = "details"
+    ul.innerText = `
     Episodes: ${element.episodes}
     Airing: ${element.airing}
-    Synopsis: ${element.synopsis} `
+    Synopsis: ${element.synopsis}`
 
     const button = document.createElement("button")
     button.id = `Anime # ${element.id} - like`
     button.className = "anime-likes"
     button.textContent =  "❤"
 
-    div.append(bg,title,span,button)
+    div.append(img,title,ul,button)
     w.append(div)
 }
+
