20d9eb5e580805c567ad7ec2abbf7a32db56402f
+ <+>
2022-07-14 10:28:33 -0400

    Added buttons and fetch function  index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,27 @@
-const m = document.querySelector('#menuSelect')
-
+const m = document.querySelectorAll(".select");
+const w = document.querySelector("#watchedlist")
 console.log(m)
+console.log(w)
+
+document.addEventListener("DOMContentLoaded",handleload)
+
+function handleload() {
+
+    fetchlist()
+
+}
+
+function fetchlist() {
+    
+    fetch("https://api.jikan.moe/v3/search/anime?q=naruto")
+    .then(resp => resp.json())
+    .then(list => handelDisplayList(list))
+    .catch()
+
+}
+
+function handelDisplayList(obj) {
+
+    console.log(obj)
+
+}
