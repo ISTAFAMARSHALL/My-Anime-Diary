7eab0ac83389dd2e8c8ef52be206e58f48f299d5
+ <+>
2022-07-14 11:19:38 -0400

    Created tiles

--- js/index.js
+++ js/index.js
@@ -20,8 +20,49 @@ function fetchlist() {
 
 }
 
+
+
 function handelDisplayList(obj) {
 
-    console.log(obj)
 
+    l = obj.results
+    l.forEach(element => {
+        
+        console.log(element.image_url)
+        console.log(element.title)
+        console.log(element.episodes)
+        
+        console.log(element.airing)
+        console.log(element.synopsis)
+        
+    });
+
+}
+
+function maketile(element) {
+
+    const div = document.createElement("div")
+    div.id = `Anime # ${list.id}`
+    div.className = 'anime-list'
+
+    const bg = document.createElement('img')
+    bg = element.image_url
+
+    const  title = document.createElement("h2")
+    title.textContent = element.title
+
+    const span = document.createElement("span")
+    span.className = "details"
+    span.textContent = `
+    Episodes: ${element.episodes}
+    Airing: ${element.airing}
+    Synopsis: ${element.synopsis} `
+
+    const button = document.createElement("button")
+    button.id = `Anime # ${list.id} - like`
+    button.className = "anime-likes"
+    button.textContent =  "❤"
+
+    div.append(bg,title,span,button)
+    animeUl().appendchild("div")
 }
